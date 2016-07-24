0b8a9f0e6ddef2c4b493e63bf1302d5e7a80c364
robert-vo <robertvo79@gmail.com>
2016-07-24 18:52:52 -0500

    Name change

--- src/test/java/com/scraper/ClassScraperAPIControllerTest.java
+++ src/test/java/com/scraper/ClassScraperAPIControllerTest.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.scraper;
 
 import com.scraper.main.Application;
@@ -30,7 +15,7 @@ import org.springframework.web.context.WebApplicationContext;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
 @WebAppConfiguration
-public class GreetingControllerTests {
+public class ClassScraperAPIControllerTest {
 
 	@Autowired
 	private WebApplicationContext ctx;
@@ -58,6 +43,6 @@ public class GreetingControllerTests {
 //				.andDo(print())
 //				.andExpect(status().isOk())
 //				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+//
 	}
-
 }
