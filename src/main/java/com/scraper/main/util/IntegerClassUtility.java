0b426881a5f30f1744c1a52b7144fff56cf00686
robert-vo <robertvo79@gmail.com>
2016-07-26 18:19:48 -0500

    Modified method to make the tests pass

--- src/main/java/com/scraper/main/util/IntegerClassUtility.java
+++ src/main/java/com/scraper/main/util/IntegerClassUtility.java
@@ -2,6 +2,12 @@ package com.scraper.main.util;
 
 public class IntegerClassUtility {
     public static int getClassHours(String classTitle) {
-        return Character.getNumericValue(classTitle.charAt(1));
+        try {
+            char classHours = classTitle.charAt(1);
+            return classHours >= '0' && classHours <= '9' ? Character.getNumericValue(classHours) : 0;
+        }
+        catch (StringIndexOutOfBoundsException | NullPointerException e) {
+            return 0;
+        }
     }
 }
