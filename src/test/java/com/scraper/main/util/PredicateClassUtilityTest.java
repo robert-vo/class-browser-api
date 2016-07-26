53f4df42e60fe5662ecca7e5cde572adfa91c168
robert-vo <robertvo79@gmail.com>
2016-07-26 18:42:39 -0500

    Added tests to get predicates to filter for terms and titles.

--- src/test/java/com/scraper/main/util/PredicateClassUtilityTest.java
+++ src/test/java/com/scraper/main/util/PredicateClassUtilityTest.java
@@ -1,8 +1,67 @@
 package com.scraper.main.util;
 
+import com.scraper.main.Class;
+import com.scraper.main.Term;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static com.scraper.main.util.PredicateClassUtility.getPredicateToFilterByTerm;
+import static com.scraper.main.util.PredicateClassUtility.getPredicateToFilterByTitle;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by Robert on 7/26/16.
  */
 public class PredicateClassUtilityTest {
+
+    Class classOne;
+    Class classTwo;
+    Class classNull;
+
+    @Before
+    public void setUp() {
+        classOne = new Class(Term.FALL_2016, "TitleA", "ONE_DEPT", "1111", Class.Status.Open, "Course", 0, 0, 0, null, null, "Core", null, null,
+                false, false, false, false, false, false, false, "Name", "Email", "Location", "PGH", "Room", "Hybrid", "Description", "15 weeks",
+                "1", "LEC", "Syllabus");
+
+        classTwo = new Class(Term.SUMMER_2016, "TitleB", "TWO_DEPT", "9999", Class.Status.Closed, "Course", 0, 0, 0, null, null, "Distance", null, null,
+                true, true, true, true, true, true, true, "Name", "Email", "Location", "SEC", "Room", "Online", "Description", "6 weeks",
+                "MIN", "LAB", "Unavailable");
+        classNull = null;
+    }
+
+    @Test
+    public void testGetPredicateToFilterByTermForValidTermFall2016() {
+        assertTrue(getPredicateToFilterByTerm("2000").test(classOne));
+    }
+
+    @Test
+    public void testGetPredicateToFilterByTermForValidTermSummer2016() {
+        assertTrue(getPredicateToFilterByTerm("1990").test(classTwo));
+    }
+
+    @Test
+    public void testGetPredicateFailsForIncorrectTerm() {
+        assertFalse(getPredicateToFilterByTerm("1234").test(classOne));
+    }
+
+    @Test
+    public void testGetPredicateToFilerByTitleForValidTermFall2016() {
+        assertTrue(getPredicateToFilterByTitle(Optional.of("titlea")).test(classOne));
+    }
+
+    @Test
+    public void testGetPredicateToFilerByTitleForValidTermSummer2016() {
+        assertTrue(getPredicateToFilterByTitle(Optional.of("titleb")).test(classTwo));
+    }
+
+    @Test
+    public void testGetPredicateToFilerByTitleForNullTerm() {
+        assertFalse(getPredicateToFilterByTitle(Optional.ofNullable(null)).test(classNull));
+    }
+
 
 }
