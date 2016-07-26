1e8667ac1139da34fe94010dde165fe9e36ef1f2
robert-vo <robertvo79@gmail.com>
2016-07-26 16:37:58 -0500

    Filtered classes by a set list of parameters.

--- src/main/java/com/scraper/main/ClassScraperAPIController.java
+++ src/main/java/com/scraper/main/ClassScraperAPIController.java
@@ -5,39 +5,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 public class ClassScraperAPIController {
 
     private ClassScraper classScraper;
     private List<Class> allClasses = new LinkedList<>();
-
-    //departmentAbbreviation = subject
-    //isMondayClass = monday
-    //isTuesdayClass = tuesday
-    //isWednesdayClass = wednesday
-    //isThursdayClass = thursday
-    //isFridayClass = friday
-    //isSaturdayClass = saturday
-    //isSundayClass = sunday
-    private List<String> validParams = Arrays.asList("term",
-            "classTitle", "subject", "departmentCourseNumber",
-            "classStatus", "courseNumber", "seatsTaken", "seatsAvailable",
-            "seatsTotal", "classStartDate", "classEndDate", "attributes",
-            "classStartTime", "classEndTime",
-            "monday",
-            "tuesdas",
-            "wednesday",
-            "thursday",
-            "friday",
-            "saturday",
-            "sunday", "instructorName", "instructorEmail", "location",
-            "buildingAbbreviation", "buildingRoomNumber", "format", "description",
-            "duration", "session", "component", "syllabus");
-
 
     ClassScraperAPIController() {
         loadAllClassesFromScraping();
@@ -49,173 +27,69 @@ public class ClassScraperAPIController {
         classScraper.startScraper();
         allClasses.addAll(classScraper.getAllClasses());
 
-//        classScraper = new ClassScraper(2016, "Summer");
-//        classScraper.setPageLimit(10);
-//        classScraper.startScraper();
-//        allClasses.addAll(classScraper.getAllClasses());
+        classScraper = new ClassScraper(2016, "Summer");
+        classScraper.setPageLimit(10);
+        classScraper.startScraper();
+        allClasses.addAll(classScraper.getAllClasses());
     }
 
     @RequestMapping(value = "/api/classes", method = RequestMethod.GET)
-    public List<Class> getAllClasses(@RequestParam(value = "term", required = true) String term,
-                                     @RequestParam Map<String,String> requestParams) throws Exception {
+    public List<Class> getAllClasses(@RequestParam(value = "term", required = true)               Optional<String>    term,
+                                       @RequestParam(value = "title", required = false)           Optional<String>    title,
+                                       @RequestParam(value = "subject", required = false)         Optional<String>    subject,
+                                       @RequestParam(value = "status", required = false)          Optional<String>    status,
+                                       @RequestParam(value = "instructorName", required = false)  Optional<String>    instructorName,
+                                       @RequestParam(value = "location", required = false)        Optional<String>    location,
+                                       @RequestParam(value = "building", required = false)        Optional<String>    building,
+                                       @RequestParam(value = "format", required = false)          Optional<String>    format,
+                                       @RequestParam(value = "duration", required = false)        Optional<String>    duration,
+                                       @RequestParam(value = "session", required = false)         Optional<String>    session,
+                                       @RequestParam(value = "component", required = false)       Optional<String>    component,
+                                       @RequestParam(value = "hours", required = false)           Optional<Integer>   hours,
+                                       @RequestParam(value = "online", required = false)          Optional<Boolean>   online,
+                                       @RequestParam(value = "core", required = false)            Optional<Boolean>   core,
+                                       @RequestParam(value = "monday", required = false)          Optional<Boolean>   isMonday,
+                                       @RequestParam(value = "tuesday", required = false)         Optional<Boolean>   isTuesday,
+                                       @RequestParam(value = "wednesday", required = false)       Optional<Boolean>   isWednesday,
+                                       @RequestParam(value = "thursday", required = false)        Optional<Boolean>   isThursday,
+                                       @RequestParam(value = "friday", required = false)          Optional<Boolean>   isFriday,
+                                       @RequestParam(value = "saturday", required = false)        Optional<Boolean>   isSaturday,
+                                       @RequestParam(value = "sunday", required = false)          Optional<Boolean>   isSunday,
+                                       @RequestParam(value = "syllabus", required = false)        Optional<Boolean>   syllabus) {
 
-        if(areAllParamsValid(requestParams)) {
-
-            Stream<Class> stream = allClasses.stream();
-
-            stream = filterByTerm(stream, term);
-
-            for (String param : requestParams.keySet()) {
-                stream = filterStreamWithParam(stream, param, requestParams.get(param));
-            }
-
-            return new ArrayList<>(stream.collect(Collectors.toList()));
-        }
-        else {
-            return null;
-        }
+        return allClasses
+                .stream()
+                .filter(filterByTerm(term))
+                .filter(e -> !title.isPresent() || e.getClassTitle().equals(title.get()))
+                .filter(e -> !subject.isPresent() || e.getDepartmentAbbreviation().equals(subject.get().toUpperCase()))
+                .filter(e -> !hours.isPresent() || getClassHours(e.getDepartmentCourseNumber()) == hours.get())
+                .filter(e -> !status.isPresent() || e.getClassStatus().toString().equals(status.get()))
+                .filter(e -> !core.isPresent() || (core.get() == Boolean.TRUE ? e.getAttributes().toUpperCase().contains("CORE") : core.get() != Boolean.FALSE || !e.getAttributes().toUpperCase().contains("CORE")))
+                .filter(e -> !online.isPresent() || (online.get() == Boolean.TRUE ? e.getAttributes().toUpperCase().contains("DISTANCE") : online.get() != Boolean.FALSE || !e.getAttributes().toUpperCase().contains("DISTANCE")))
+                .filter(e -> !isMonday.isPresent() || e.isMondayClass())
+                .filter(e -> !isTuesday.isPresent() || e.isTuesdayClass())
+                .filter(e -> !isWednesday.isPresent() || e.isWednesdayClass())
+                .filter(e -> !isThursday.isPresent() || e.isThursdayClass())
+                .filter(e -> !isFriday.isPresent() || e.isFridayClass())
+                .filter(e -> !isSaturday.isPresent() || e.isSaturdayClass())
+                .filter(e -> !isSunday.isPresent() || e.isSundayClass())
+                .filter(e -> !instructorName.isPresent() || e.getInstructorName().equals(instructorName.get()))
+                .filter(e -> !location.isPresent() || e.getLocation().equals(location.get()))
+                .filter(e -> !building.isPresent() || e.getBuildingAbbreviation().equals(building.get()))
+                .filter(e -> !format.isPresent() || e.getFormat().equals(format.get()))
+                .filter(e -> !duration.isPresent() || e.getDuration().equals(duration.get()))
+                .filter(e -> !session.isPresent() || (session.get().equals("1") ? e.getSession().equals("Regular Academic Session") : e.getSession().equals(session.get())))
+                .filter(e -> !component.isPresent() || e.getComponent().equals(component.get()))
+                .filter(e -> !syllabus.isPresent() || (syllabus.get() == Boolean.TRUE ? !e.getSyllabus().equals("Unavailable") : syllabus.get() == Boolean.FALSE && e.getSyllabus().equals("Unavailable")))
+                .collect(Collectors.toList());
     }
 
-    private Stream<Class> filterStreamWithParam(Stream<Class> stream, final String param, final String paramValue) throws Exception {
-        switch (param) {
-            case "session":
-                if (paramValue.equals("1")) {
-                    String newParamValue = "Regular Academic Session";
-                    return stream.filter(e -> e.getSession().equals(newParamValue));
-                }
-                return stream.filter(e -> e.getSession().equals(paramValue));
-            case "subject":
-                return stream.filter(e -> e.getDepartmentAbbreviation().equals(paramValue));
-            case "seatsTaken":
-                return stream.filter(e -> e.getSeatsTaken() < Integer.parseInt(paramValue));
-            case "seatsAvailable":
-                return stream.filter(e -> e.getSeatsAvailable() < Integer.parseInt(paramValue));
-            case "seatsTotal":
-                return stream.filter(e -> e.getSeatsTotal() < Integer.parseInt(paramValue));
-            case "departmentCourseNumber":
-                return stream.filter(e -> e.getDepartmentCourseNumber().equals(paramValue));
-            case "attributes":
-                return stream.filter(e -> e.getAttributes().contains(paramValue));
-            case "location":
-                return stream.filter(e -> e.getLocation().contains(paramValue));
-            case "classStatus":
-                return stream.filter(e -> e.getClassStatus().name().equals(paramValue));
-            case "instructorName":
-                return stream.filter(e -> e.getInstructorName().equals(paramValue));
-            case "instructorEmail":
-                return stream.filter(e -> e.getInstructorEmail().equals(paramValue));
-            case "buildingAbbreviation":
-                return stream.filter(e -> e.getBuildingAbbreviation().equals(paramValue));
-            case "buildingRoomNumber":
-                return stream.filter(e -> e.getBuildingRoomNumber().equals(paramValue));
-            case "format":
-                return stream.filter(e -> e.getFormat().equals(paramValue));
-            case "duration":
-                return stream.filter(e -> e.getDuration().contains(paramValue));
-            case "component":
-                return stream.filter(e -> e.getComponent().equals(paramValue));
-            case "syllabus":
-                if(paramValue.toUpperCase().equals("UNAVAILABLE")) {
-                    return stream.filter(e -> e.getSyllabus().equals("Unavailable"));
-                }
-                else if(paramValue.toUpperCase().equals("1") || paramValue.toUpperCase().equals("AVAILABLE")){
-                    return stream.filter(e -> !e.getSyllabus().equals("Unavailable"));
-                }
-                else {
-                    System.out.println("Invalid syllabus parameter");
-                }
-            case "monday":
-                if(paramValue.equals("false")) {
-                    return stream.filter(e -> !e.isMondayClass());
-                }
-                else if(paramValue.equals("true")) {
-                    return stream.filter(e -> e.isMondayClass());
-                }
-                else {
-                    System.out.println("Invalid day parameter");
-                }
-            case "tuesday":
-                if(paramValue.equals("false")) {
-                    return stream.filter(e -> !e.isTuesdayClass());
-                }
-                else if(paramValue.equals("true")) {
-                    return stream.filter(e -> e.isTuesdayClass());
-                }
-                else {
-                    System.out.println("Invalid day parameter");
-                }
-            case "wednesday":
-                if(paramValue.equals("false")) {
-                    return stream.filter(e -> !e.isWednesdayClass());
-                }
-                else if(paramValue.equals("true")) {
-                    return stream.filter(e -> e.isWednesdayClass());
-                }
-                else {
-                    System.out.println("Invalid day parameter");
-                }
-            case "thursday":
-                if(paramValue.equals("false")) {
-                    return stream.filter(e -> !e.isThursdayClass());
-                }
-                else if(paramValue.equals("true")) {
-                    return stream.filter(e -> e.isThursdayClass());
-                }
-                else {
-                    System.out.println("Invalid day parameter");
-                }
-            case "friday":
-                if(paramValue.equals("false")) {
-                    return stream.filter(e -> !e.isFridayClass());
-                }
-                else if(paramValue.equals("true")) {
-                    return stream.filter(e -> e.isFridayClass());
-                }
-                else {
-                    System.out.println("Invalid day parameter");
-                }
-
-            case "saturday":
-                if(paramValue.equals("false")) {
-                    return stream.filter(e -> !e.isSaturdayClass());
-                }
-                else if(paramValue.equals("true")) {
-                    return stream.filter(e -> e.isSaturdayClass());
-                }
-                else {
-                    System.out.println("Invalid day parameter");
-                }
-
-            case "sunday":
-                return filterBySunday(stream, paramValue);
-            default:
-                return stream;
-        }
+    private int getClassHours(String classTitle) {
+        return Character.getNumericValue(classTitle.charAt(1));
     }
 
-    private Stream<Class> filterByTerm(Stream<Class> stream, String term) {
-        return stream.filter(e -> e.getTerm().getTermID().equals(term));
-    }
-
-    private Stream<Class> filterBySunday(Stream<Class> stream, String exists) throws Exception {
-        switch (exists) {
-            case "false":
-                return stream.filter(e -> !e.isSundayClass());
-            case "true":
-                return stream.filter(Class::isSundayClass);
-            default:
-                throw new Exception("Invalid parameter for sunday");
-        }
-    }
-
-    private boolean areAllParamsValid(Map<String, String> params) {
-        for (String param : params.keySet()) {
-            if(!validParams.contains(param)) {
-                System.out.println(param + " is invalid.");
-                return false;
-            }
-        }
-        return true;
+    private Predicate<Class> filterByTerm(Optional<String> term) {
+        return aClass -> !term.isPresent() || aClass.getTerm().getTermID().equals(term.get());
     }
 
 }
