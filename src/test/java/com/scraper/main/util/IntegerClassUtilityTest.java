a171fe3d249679eaa914c7a348f2a385ac167f82
robert-vo <robertvo79@gmail.com>
2016-07-26 18:19:38 -0500

    Added empty, null, and valid tests

--- src/test/java/com/scraper/main/util/IntegerClassUtilityTest.java
+++ src/test/java/com/scraper/main/util/IntegerClassUtilityTest.java
@@ -1,8 +1,43 @@
 package com.scraper.main.util;
 
-/**
- * Created by Robert on 7/26/16.
- */
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class IntegerClassUtilityTest {
 
+    @Test
+    public void testGetClassHoursForValidCourse1300() {
+        assertEquals(IntegerClassUtility.getClassHours("1300"), 3);
+    }
+
+    @Test
+    public void testGetClassHoursForValidCourse1400() {
+        assertEquals(IntegerClassUtility.getClassHours("1400"), 4);
+    }
+
+    @Test
+    public void testGetClassHoursForValidCourse1200() {
+        assertEquals(IntegerClassUtility.getClassHours("1200"), 2);
+    }
+
+    @Test
+    public void testGetClassHoursForValidCourse1100() {
+        assertEquals(IntegerClassUtility.getClassHours("1100"), 1);
+    }
+
+    @Test
+    public void testGetClassHoursForInvalidCourseXXXX() {
+        assertEquals(IntegerClassUtility.getClassHours("XXXX"), 0);
+    }
+
+    @Test
+    public void testGetClassHoursForNullCourse() {
+        assertEquals(IntegerClassUtility.getClassHours(null), 0);
+    }
+
+    @Test
+    public void testGetClassHoursForEmptyCourse() {
+        assertEquals(IntegerClassUtility.getClassHours(""), 0);
+    }
 }
