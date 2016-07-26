b81276625fa969aee47727530cb717ae92ef5f16
robert-vo <robertvo79@gmail.com>
2016-07-26 18:11:38 -0500

    Removed predicate logic to PredicateClassUtility

--- src/main/java/com/scraper/main/ClassScraperAPIController.java
+++ src/main/java/com/scraper/main/ClassScraperAPIController.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
+import static com.scraper.main.util.PredicateClassUtility.*;
 
 @RestController
 public class ClassScraperAPIController {
@@ -29,13 +30,13 @@ public class ClassScraperAPIController {
 //        allClasses.addAll(classScraper.getAllClasses());
 
         classScraper = new ClassScraper(2016, "Summer");
-//        classScraper.setPageLimit(1);
+        classScraper.setPageLimit(1);
         classScraper.startScraper();
         allClasses.addAll(classScraper.getAllClasses());
     }
 
     @RequestMapping(value = "/api/classes", method = RequestMethod.GET)
-    public List<Class> getAllClasses(@RequestParam(value = "term", required = true)             Optional<String>    term,
+    public List<Class> getAllClasses(@RequestParam(value = "term", required = true)             String              term,
                                      @RequestParam(value = "title", required = false)           Optional<String>    title,
                                      @RequestParam(value = "subject", required = false)         Optional<String>    subject,
                                      @RequestParam(value = "instructorName", required = false)  Optional<String>    instructorName,
@@ -59,7 +60,7 @@ public class ClassScraperAPIController {
                                      @RequestParam(value = "syllabus", required = false)        Optional<Boolean>   syllabus) {
 
         return allClasses
-                .stream()
+                .parallelStream()
                 .filter(getPredicateToFilterByTerm(term))
                 .filter(getPredicateToFilterByTitle(title))
                 .filter(getPredicateToFilterBySubject(subject))
@@ -83,114 +84,6 @@ public class ClassScraperAPIController {
                 .filter(getPredicateToFilterByComponent(component))
                 .filter(getPredicateToFilterBySyllabus(syllabus))
                 .collect(Collectors.toList());
-    }
-
-    private int getClassHours(String classTitle) {
-        return Character.getNumericValue(classTitle.charAt(1));
-    }
-
-    private Predicate<Class> getPredicateToFilterByTerm(Optional<String> term) {
-        return e -> !term.isPresent() || e.getTerm().getTermID().equals(term.get());
-    }
-
-    private Predicate<Class> getPredicateToFilterByTitle(Optional<String> title) {
-        return e -> !title.isPresent() || e.getClassTitle().toUpperCase().equals(title.get().toUpperCase());
-    }
-
-    private Predicate<Class> getPredicateToFilterBySubject(Optional<String> subject) {
-        return e -> !subject.isPresent() || e.getDepartmentAbbreviation().toUpperCase().equals(subject.get().toUpperCase());
-    }
-
-    private Predicate<Class> getPredicateToFilterByHours(Optional<Integer> hours) {
-        return e -> !hours.isPresent() || getClassHours(e.getDepartmentCourseNumber()) == hours.get();
-    }
-
-    private Predicate<Class> getPredicateToFilterByStatus(Optional<Boolean> status) {
-        return e -> !status.isPresent() || (status.get() == Boolean.TRUE ? e.getClassStatus().toString().equals("OPEN") : e.getClassStatus().toString().equals("CLOSED"));
-    }
-
-    private Predicate<Class> getPredicateToFilterByInstructorName(Optional<String> instructorName) {
-        return aClass -> !instructorName.isPresent() || aClass.getInstructorName().equals(instructorName.get());
-    }
-
-    private Predicate<Class> getPredicateToFilterByLocation(Optional<String> location) {
-        return e -> !location.isPresent() || e.getLocation().equals(location.get());
-    }
-
-    private Predicate<Class> getPredicateToFilterByAttribute(Optional<Boolean> doesExist, String attributeType) {
-        return e ->
-                !doesExist.isPresent() ||
-                        (doesExist.get() == Boolean.TRUE ? e.getAttributes().toUpperCase().contains(attributeType) :
-                                doesExist.get() != Boolean.FALSE || !e.getAttributes().toUpperCase().contains(attributeType));
-    }
-
-    private Predicate<Class> getPredicateToFilterByBuilding(Optional<String> building) {
-        return e -> !building.isPresent() || e.getBuildingAbbreviation().toUpperCase().equals(building.get().toUpperCase());
-    }
-
-    private Predicate<Class> getPredicateToFilterByMonday(Optional<Boolean> isMonday) {
-        return e -> !isMonday.isPresent() || e.isMondayClass();
-    }
-
-    private Predicate<Class> getPredicateToFilterByTuesday(Optional<Boolean> isTuesday) {
-        return e -> !isTuesday.isPresent() || e.isTuesdayClass();
-    }
-
-    private Predicate<Class> getPredicateToFilterByWednesday(Optional<Boolean> isWednesday) {
-        return e -> !isWednesday.isPresent() || e.isWednesdayClass();
-    }
-
-    private Predicate<Class> getPredicateToFilterByThursday(Optional<Boolean> isThursday) {
-        return e -> !isThursday.isPresent() || e.isThursdayClass();
-    }
-
-    private Predicate<Class> getPredicateToFilterByFriday(Optional<Boolean> isFriday) {
-        return e -> !isFriday.isPresent() || e.isFridayClass();
-    }
-
-    private Predicate<Class> getPredicateToFilterBySaturday(Optional<Boolean> isSaturday) {
-        return e -> !isSaturday.isPresent() || e.isSaturdayClass();
-    }
-
-    private Predicate<Class> getPredicateToFilterBySunday(Optional<Boolean> isSunday) {
-        return e -> !isSunday.isPresent() || e.isSundayClass();
-    }
-
-    /**
-     * Options include
-     * Face to Face
-     * Online
-     * Hybrid
-     */
-    private Predicate<Class> getPredicateToFilterByFormat(Optional<String> format) {
-        return e -> !format.isPresent() || e.getFormat().toUpperCase().equals(format.get().toUpperCase());
-    }
-
-    private Predicate<Class> getPredicateToFilterByDuration(Optional<String> duration) {
-        return e -> !duration.isPresent() || e.getDuration().equals(duration.get());
-    }
-
-    private Predicate<Class> getPredicateToFilterBySession(Optional<String> session) {
-        return e -> !session.isPresent() ||
-                (session.get().equals("1") ?
-                        e.getSession().equals("Regular Academic Session") :
-                        e.getSession().equals(session.get()));
-    }
-
-    /**
-     * Options include
-     * LAB
-     * LEC
-     */
-    private Predicate<Class> getPredicateToFilterByComponent(Optional<String> component) {
-        return e -> !component.isPresent() || e.getComponent().toUpperCase().equals(component.get().toUpperCase());
-    }
-
-    private Predicate<Class> getPredicateToFilterBySyllabus(Optional<Boolean> syllabus) {
-        return e -> !syllabus.isPresent() ||
-                (syllabus.get() == Boolean.TRUE ?
-                        !e.getSyllabus().equals("Unavailable") :
-                        syllabus.get() == Boolean.FALSE && e.getSyllabus().equals("Unavailable"));
     }
 
 }
