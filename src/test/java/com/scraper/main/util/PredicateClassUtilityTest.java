d80ea90f251e4f7fb878f5670a85e0503614fd1e
robert-vo <robertvo79@gmail.com>
2016-07-26 19:20:54 -0500

    Added tests for location, core attributes, online attributes and buildings.

--- src/test/java/com/scraper/main/util/PredicateClassUtilityTest.java
+++ src/test/java/com/scraper/main/util/PredicateClassUtilityTest.java
@@ -19,15 +19,17 @@ public class PredicateClassUtilityTest {
     Class classOne;
     Class classTwo;
     Class classNull;
+    final String CORE = "CORE";
+    final String ONLINE = "DISTANCE";
 
     @Before
     public void setUp() {
         classOne = new Class(Term.FALL_2016, "TitleA", "ONE_DEPT", "1111", Class.Status.Open, "Course", 0, 0, 0, null, null, "Core", null, null,
-                false, false, false, false, false, false, false, "classonename", "Email", "Location", "PGH", "Room", "Hybrid", "Description", "15 weeks",
+                false, false, false, false, false, false, false, "classonename", "Email", "UH", "PGH", "Room", "Hybrid", "Description", "15 weeks",
                 "1", "LEC", "Syllabus");
 
         classTwo = new Class(Term.SUMMER_2016, "TitleB", "TWO_DEPT", "9999", Class.Status.Closed, "Course", 0, 0, 0, null, null, "Distance", null, null,
-                true, true, true, true, true, true, true, "classtwoname", "Email", "Location", "SEC", "Room", "Online", "Description", "6 weeks",
+                true, true, true, true, true, true, true, "classtwoname", "Email", "UHV", "SEC", "Room", "Online", "Description", "6 weeks",
                 "MIN", "LAB", "Unavailable");
         classNull = null;
     }
@@ -120,6 +122,66 @@ public class PredicateClassUtilityTest {
     @Test
     public void testGetPredicateToFilterByInstructorNameForNullTerm() {
         assertTrue(getPredicateToFilterByInstructorName(Optional.ofNullable(null)).test(classNull));
+    }
+
+    @Test
+    public void testGetPredicateToFilterByLocationForValidTermFall2016() {
+        assertTrue(getPredicateToFilterByLocation(Optional.of("UH")).test(classOne));
+    }
+
+    @Test
+    public void testGetPredicateToFilterByLocationForValidTermSummer2016() {
+        assertTrue(getPredicateToFilterByLocation(Optional.of("UHV")).test(classTwo));
+    }
+
+    @Test
+    public void testGetPredicateToFilterByLocationForNullTerm() {
+        assertTrue(getPredicateToFilterByLocation(Optional.ofNullable(null)).test(classNull));
+    }
+
+    @Test
+    public void testGetPredicateToFilterByCoreAttributeForValidTermFall2016() {
+        assertTrue(getPredicateToFilterByAttribute(Optional.of(Boolean.TRUE), CORE).test(classOne));
+    }
+
+    @Test
+    public void testGetPredicateToFilterByCoreAttributeForValidTermSummer2016() {
+        assertTrue(getPredicateToFilterByAttribute(Optional.of(Boolean.FALSE), CORE).test(classTwo));
+    }
+
+    @Test
+    public void testGetPredicateToFilterByCoreAttributeForNullTerm() {
+        assertTrue(getPredicateToFilterByAttribute(Optional.ofNullable(null), CORE).test(classNull));
+    }
+
+    @Test
+    public void testGetPredicateToFilterByOnlineAttributeForValidTermFall2016() {
+        assertTrue(getPredicateToFilterByAttribute(Optional.of(Boolean.FALSE), ONLINE).test(classOne));
+    }
+
+    @Test
+    public void testGetPredicateToFilterByOnlineAttributeForValidTermSummer2016() {
+        assertTrue(getPredicateToFilterByAttribute(Optional.of(Boolean.TRUE), ONLINE).test(classTwo));
+    }
+
+    @Test
+    public void testGetPredicateToFilterByOnlineAttributeForNullTerm() {
+        assertTrue(getPredicateToFilterByAttribute(Optional.ofNullable(null), ONLINE).test(classNull));
+    }
+
+    @Test
+    public void testGetPredicateToFilterByBuildingForValidTermFall2016() {
+        assertTrue(getPredicateToFilterByBuilding(Optional.of("PGH")).test(classOne));
+    }
+
+    @Test
+    public void testGetPredicateToFilterByBuildingForValidTermSummer2016() {
+        assertTrue(getPredicateToFilterByBuilding(Optional.of("SEC")).test(classTwo));
+    }
+
+    @Test
+    public void testGetPredicateToFilterByBuildingForNullTerm() {
+        assertTrue(getPredicateToFilterByBuilding(Optional.ofNullable(null)).test(classNull));
     }
 
 }
