d10b57c818ca699b485cfbd834974b202f9a680f
robert-vo <robertvo79@gmail.com>
2016-07-27 18:24:28 -0500

    Initial commit - prints out invalid parameters for an error page.

---
+++ src/main/java/com/scraper/main/IndexController.java
@@ -0,0 +1,24 @@
+package com.scraper.main;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Robert on 7/27/16.
+ */
+@RestController
+public class IndexController implements ErrorController {
+
+    private static final String ERROR_PATH = "/error";
+
+    @RequestMapping(value = ERROR_PATH)
+    public String printError() {
+        return "Invalid parameters";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+}
