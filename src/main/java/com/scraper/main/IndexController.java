8e74b36dc5c6fc35d14f11895ba7eb3070670bdc
robert-vo <robertvo79@gmail.com>
2016-08-01 16:31:13 -0500

    Redirected error page to the static error page

--- src/main/java/com/scraper/main/IndexController.java
+++ src/main/java/com/scraper/main/IndexController.java
@@ -2,6 +2,7 @@ package com.scraper.main;
 
 import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,13 +13,18 @@ public class IndexController implements ErrorController {
 
     private static final String ERROR_PATH = "/error";
 
-    @RequestMapping(value = ERROR_PATH)
-    public String printError() {
-        return "Invalid parameters";
-    }
+//    @RequestMapping(value = ERROR_PATH)
+//    public String printError() {
+//        return "Invalid parameters";
+//    }
 
     @Override
     public String getErrorPath() {
         return ERROR_PATH;
+    }
+
+    @RequestMapping(value = ERROR_PATH, method = RequestMethod.GET)
+    public String redirect() {
+        return "redirect:/pages/error.htm";
     }
 }
