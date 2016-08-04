dda962d8c113a1d711e6678566201d5a6e6b6b63
robert-vo <robertvo79@gmail.com>
2016-08-04 01:12:56 -0500

    Removed getAllClasses in favor of a stateless restful api

--- src/main/java/com/scraper/main/ClassScraperAPIController.java
+++ src/main/java/com/scraper/main/ClassScraperAPIController.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.*;
-import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -17,7 +16,6 @@ import static com.scraper.main.util.PredicateClassUtility.*;
 public class ClassScraperAPIController {
 
     private ClassScraper classScraper;
-    private HashSet<ClassInformation> allClassInformations = new HashSet<>();
     private HashSet<Class> allClasses = new HashSet<>();
     static Properties properties = new Properties();
     static String jdbcDriver;
@@ -69,65 +67,11 @@ public class ClassScraperAPIController {
 
                 classScraper.startScraper();
                 allClasses.addAll(classScraper.getAllClasses());
-                updateTimeStamp();
             }
         };
 
 //        timer.schedule (hourlyTask, 60*60*24, 60*60*24*100);
 
-    }
-
-    @RequestMapping(value = "/a1pi", method = RequestMethod.GET)
-    @ResponseBody
-    public List<Class> getAllClasses(@RequestParam(value = "term", required = true)             String              term,
-                                                @RequestParam(value = "title", required = false)           Optional<String>    title,
-                                                @RequestParam(value = "subject", required = false)         Optional<String>    subject,
-                                                @RequestParam(value = "instructorName", required = false)  Optional<String>    instructorName,
-                                                @RequestParam(value = "location", required = false)        Optional<String>    location,
-                                                @RequestParam(value = "building", required = false)        Optional<String>    building,
-                                                @RequestParam(value = "format", required = false)          Optional<String>    format,
-                                                @RequestParam(value = "duration", required = false)        Optional<String>    duration,
-                                                @RequestParam(value = "session", required = false)         Optional<String>    session,
-                                                @RequestParam(value = "component", required = false)       Optional<String>    component,
-                                                @RequestParam(value = "hours", required = false)           Optional<Integer>   hours,
-                                                @RequestParam(value = "status", required = false)          Optional<Boolean>   status,
-                                                @RequestParam(value = "online", required = false)          Optional<Boolean>   online,
-                                                @RequestParam(value = "core", required = false)            Optional<Boolean>   core,
-                                                @RequestParam(value = "monday", required = false)          Optional<Boolean>   isMonday,
-                                                @RequestParam(value = "tuesday", required = false)         Optional<Boolean>   isTuesday,
-                                                @RequestParam(value = "wednesday", required = false)       Optional<Boolean>   isWednesday,
-                                                @RequestParam(value = "thursday", required = false)        Optional<Boolean>   isThursday,
-                                                @RequestParam(value = "friday", required = false)          Optional<Boolean>   isFriday,
-                                                @RequestParam(value = "saturday", required = false)        Optional<Boolean>   isSaturday,
-                                                @RequestParam(value = "sunday", required = false)          Optional<Boolean>   isSunday,
-                                                @RequestParam(value = "syllabus", required = false)        Optional<Boolean>   syllabus) {
-
-        return new ArrayList<>(allClasses
-                .parallelStream()
-                .filter(getPredicateToFilterByTerm(term))
-                .filter(getPredicateToFilterByTitle(title))
-                .filter(getPredicateToFilterBySubject(subject))
-                .filter(getPredicateToFilterByHours(hours))
-                .filter(getPredicateToFilterByStatus(status))
-                .filter(getPredicateToFilterByAttribute(core, "CORE"))
-                .filter(getPredicateToFilterByAttribute(online, "ONLINE"))
-                .filter(getPredicateToFilterByMonday(isMonday))
-                .filter(getPredicateToFilterByTuesday(isTuesday))
-                .filter(getPredicateToFilterByWednesday(isWednesday))
-                .filter(getPredicateToFilterByThursday(isThursday))
-                .filter(getPredicateToFilterByFriday(isFriday))
-                .filter(getPredicateToFilterBySaturday(isSaturday))
-                .filter(getPredicateToFilterBySunday(isSunday))
-                .filter(getPredicateToFilterByInstructorName(instructorName))
-                .filter(getPredicateToFilterByLocation(location))
-                .filter(getPredicateToFilterByBuilding(building))
-                .filter(getPredicateToFilterByFormat(format))
-                .filter(getPredicateToFilterByDuration(duration))
-                .filter(getPredicateToFilterBySession(session))
-                .filter(getPredicateToFilterByComponent(component))
-                .filter(getPredicateToFilterBySyllabus(syllabus))
-//                .map(this::injectCurrentTime)
-                .collect(Collectors.toList()));
     }
 
     @RequestMapping(value = "/term={term}", method = RequestMethod.GET)
