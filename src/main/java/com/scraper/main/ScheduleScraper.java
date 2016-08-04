dae21c4441949e43cfead96ce875956ac2225d26
robert-vo <robertvo79@gmail.com>
2016-08-04 02:30:31 -0500

    Implemented updateClasses which should run at midnight every day, central time.

--- src/main/java/com/scraper/main/ScheduleScraper.java
+++ src/main/java/com/scraper/main/ScheduleScraper.java
@@ -3,13 +3,29 @@ package com.scraper.main;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class ScheduleScraper {
 
-    @Scheduled(cron = "0 0 * * * 0,1,2,3,4,5,6")
+    /**
+     * Runs Scraper for Summer 2016 and Fall 2016 classes at midnight.
+     */
+    @Scheduled(cron = "0 0 0 * * *", zone="America/Chicago")
     public void updateClasses() {
-        //Scrape Classes
-        //Update database
+
+        System.out.println("Updating classes...");
+
+        List<Term> terms = new ArrayList<>(Arrays.asList(Term.SUMMER_2016, Term.FALL_2016));
+        ClassScraper classScraper = new ClassScraper(terms);
+        Optional<String> envVariable = Optional.ofNullable(System.getenv("pageLimit"));
+        classScraper.setPageLimit(envVariable.isPresent() ? Integer.parseInt(envVariable.get()) : 1);
+        classScraper.startScraperForMultipleTerms();
+        DatabaseOperations.setPropertiesFileLocation("config/config.properties");
+        DatabaseOperations.performDatabaseActions(classScraper.getAllClasses());
     }
 
 }
