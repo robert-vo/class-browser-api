54b6506b6161f55d497a975d27cdd53cd6bf0c48
robert-vo <robertvo79@gmail.com>
2016-08-10 01:55:21 -0500

    Removed SQL query generation logic to StringSQLQueryUtility.java

--- src/main/java/com/scraper/main/ClassScraperAPIController.java
+++ src/main/java/com/scraper/main/ClassScraperAPIController.java
@@ -1,5 +1,6 @@
 package com.scraper.main;
 
+import com.scraper.main.util.StringSQLQueryUtility;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.FileInputStream;
@@ -47,6 +48,7 @@ public class ClassScraperAPIController {
             PreparedStatement preparedStatement = conn.prepareStatement(SQL_QUERY_CORE_CLASSES);
             preparedStatement.setString(1, core);
             preparedStatement.setString(2, core);
+            preparedStatement.setString(3, core);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 CoreClassInformation c;
@@ -73,7 +75,8 @@ public class ClassScraperAPIController {
     public List<ClassInformation> getAllClassesFromTerm(@PathVariable(value = "term") String term,
                                                         @RequestParam Map<String, String> params) {
         List<ClassInformation> allClassInformation = new LinkedList<>();
-        final String SQL_QUERY_ALL_CLASSES = buildSqlQuery(params);
+
+        final String SQL_QUERY_ALL_CLASSES = StringSQLQueryUtility.buildSqlQuery(params);
 
         handleJavaLangClassDriver();
 
@@ -91,86 +94,6 @@ public class ClassScraperAPIController {
         return allClassInformation;
     }
 
-    private String buildSqlQuery(Map<String, String> params) {
-        final String SQL_QUERY_FOR_ALL_TERMS = "SELECT * FROM class, building, department, terms, class_information " +
-                "WHERE class.TERM_ID = ? AND " +
-                "building.building_abbreviation = class.building_abbv AND " +
-                "department.department_abbreviation = class.department AND " +
-                "terms.term_id = class.term_id AND " +
-                "class.department_crn = class_information.department_crn ";
-        StringBuilder sqlQuery = new StringBuilder(SQL_QUERY_FOR_ALL_TERMS);
-
-        for (String s : params.keySet()) {
-            if (s.equalsIgnoreCase("online")) {
-                if (params.get(s).equals("1")) {
-                    sqlQuery.append("AND class.format = 'Online' ");
-                } else if (params.get(s).equals("0")) {
-                    sqlQuery.append("AND class.format <> 'Online' ");
-                }
-            }
-            else if (s.equalsIgnoreCase("hybrid")) {
-                if (params.get(s).equals("1")) {
-                    sqlQuery.append("AND class.format = 'Hybrid' ");
-                } else if (params.get(s).equals("0")) {
-                    sqlQuery.append("AND class.format <> 'Hybrid' ");
-                }
-            }
-            else if (s.equalsIgnoreCase("facetoface")) {
-                if (params.get(s).equals("1")) {
-                    sqlQuery.append("AND class.format = 'Face to Face' ");
-                } else if (params.get(s).equals("0")) {
-                    sqlQuery.append("AND class.format <> 'Face to Face' ");
-                }
-            }
-            else if (s.equalsIgnoreCase("status")) {
-                if (params.get(s).equalsIgnoreCase("open") || params.get(s).equalsIgnoreCase("1")) {
-                    sqlQuery.append("AND class.status = 'Open' ");
-                }
-                else if (params.get(s).equalsIgnoreCase("closed") || params.get(s).equalsIgnoreCase("0")) {
-                    sqlQuery.append("AND class.status = 'Closed' ");
-                }
-            }
-            else if (s.equalsIgnoreCase("session")) {
-                if (params.get(s).equals("1")) {
-                    sqlQuery.append("AND class.session = 'Regular Academic Session' ");
-                }
-                else if (params.get(s).equals("MIN")) {
-                    sqlQuery.append("AND class.session = 'MIN' ");
-                }
-                else {
-                    sqlQuery.append("AND class.session = ").append(params.get(s)).append(" ");
-                }
-            }
-            else if (s.equalsIgnoreCase("department")) {
-                sqlQuery.append("AND class.department = '").append(params.get(s)).append("' ");
-            }
-            else if (s.equalsIgnoreCase("department_crn")) {
-                sqlQuery.append("AND class.department_crn = '").append(params.get(s)).append("' ");
-            }
-            else if (s.equalsIgnoreCase("location")) {
-                sqlQuery.append("AND class.location = '").append(params.get(s)).append("' ");
-            }
-            else if (s.equalsIgnoreCase("component")) {
-                sqlQuery.append("AND class.component = '").append(params.get(s)).append("' ");
-            }
-            else if (s.equalsIgnoreCase("building")) {
-                sqlQuery.append("AND building_abbreviation = '").append(params.get(s)).append("' ");
-            }
-            else if (s.equalsIgnoreCase("credit_hours")) {
-                sqlQuery.append("AND credit_hours = '").append(params.get(s)).append("' ");
-            }
-            else if (s.equalsIgnoreCase("core")) {
-                sqlQuery.append("AND (core = ")
-                        .append(params.get(s))
-                        .append(" or core like '")
-                        .append(params.get(s))
-                        .append(",%' or core like '%,")
-                        .append(params.get(s))
-                        .append("') ");
-            }
-        }
-        return sqlQuery.toString();
-    }
     private void handleJavaLangClassDriver() {
         try {
             java.lang.Class.forName(jdbcDriver);
