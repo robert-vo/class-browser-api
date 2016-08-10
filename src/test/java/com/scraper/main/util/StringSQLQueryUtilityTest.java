bc862c7a9a759db4304222445d67f99b9a3e783a
robert-vo <robertvo79@gmail.com>
2016-08-10 12:35:53 -0500

    Added tests for sessions and status

--- src/test/java/com/scraper/main/util/StringSQLQueryUtilityTest.java
+++ src/test/java/com/scraper/main/util/StringSQLQueryUtilityTest.java
@@ -58,11 +58,76 @@ public class StringSQLQueryUtilityTest {
         assertEquals(StringSQLQueryUtility.buildSqlQuery(paramMap), SQL_QUERY_FOR_ALL_TERMS + "AND class.format = 'Face To Face'");
     }
 
-
     @Test
     public void testBuildSqlQueryForFaceToFaceFilterFalse() {
         paramMap.put("FaceTOFACE", "false");
         assertEquals(StringSQLQueryUtility.buildSqlQuery(paramMap), SQL_QUERY_FOR_ALL_TERMS + "AND class.format <> 'Face To Face'");
+    }
+
+    @Test
+    public void testBuildSqlQueryForStatusFilterOpen1() {
+        paramMap.put("status", "open");
+        assertEquals(StringSQLQueryUtility.buildSqlQuery(paramMap), SQL_QUERY_FOR_ALL_TERMS + "AND class.status = 'Open'");
+    }
+
+    @Test
+    public void testBuildSqlQueryForStatusFilterOpen2() {
+        paramMap.put("status", "true");
+        assertEquals(StringSQLQueryUtility.buildSqlQuery(paramMap), SQL_QUERY_FOR_ALL_TERMS + "AND class.status = 'Open'");
+    }
+
+    @Test
+    public void testBuildSqlQueryForStatusFilterClosed1() {
+        paramMap.put("status", "closed");
+        assertEquals(StringSQLQueryUtility.buildSqlQuery(paramMap), SQL_QUERY_FOR_ALL_TERMS + "AND class.status = 'Closed'");
+    }
+
+    @Test
+    public void testBuildSqlQueryForStatusFilterClosed2() {
+        paramMap.put("status", "0");
+        assertEquals(StringSQLQueryUtility.buildSqlQuery(paramMap), SQL_QUERY_FOR_ALL_TERMS + "AND class.status = 'Closed'");
+    }
+
+    @Test
+    public void testBuildSqlQueryForSession1() {
+        paramMap.put("session", "1");
+        assertEquals(StringSQLQueryUtility.buildSqlQuery(paramMap), SQL_QUERY_FOR_ALL_TERMS + "AND class.session = 'Regular Academic Session'");
+    }
+
+    @Test
+    public void testBuildSqlQueryForSession2() {
+        paramMap.put("session", "2");
+        assertEquals(StringSQLQueryUtility.buildSqlQuery(paramMap), SQL_QUERY_FOR_ALL_TERMS + "AND class.session = 2");
+    }
+
+    @Test
+    public void testBuildSqlQueryForSession3() {
+        paramMap.put("session", "3");
+        assertEquals(StringSQLQueryUtility.buildSqlQuery(paramMap), SQL_QUERY_FOR_ALL_TERMS + "AND class.session = 3");
+    }
+
+    @Test
+    public void testBuildSqlQueryForSession4() {
+        paramMap.put("session", "4");
+        assertEquals(StringSQLQueryUtility.buildSqlQuery(paramMap), SQL_QUERY_FOR_ALL_TERMS + "AND class.session = 4");
+    }
+
+    @Test
+    public void testBuildSqlQueryForSession5() {
+        paramMap.put("session", "5");
+        assertEquals(StringSQLQueryUtility.buildSqlQuery(paramMap), SQL_QUERY_FOR_ALL_TERMS + "AND class.session = 5");
+    }
+
+    @Test
+    public void testBuildSqlQueryForSession6() {
+        paramMap.put("session", "6");
+        assertEquals(StringSQLQueryUtility.buildSqlQuery(paramMap), SQL_QUERY_FOR_ALL_TERMS + "AND class.session = 6");
+    }
+
+    @Test
+    public void testBuildSqlQueryForSessionMIN() {
+        paramMap.put("session", "MIN");
+        assertEquals(StringSQLQueryUtility.buildSqlQuery(paramMap), SQL_QUERY_FOR_ALL_TERMS + "AND class.session = 'MIN'");
     }
 
 
