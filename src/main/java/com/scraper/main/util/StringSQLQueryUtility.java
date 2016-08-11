fde9ab3ba63ba8b05461b069598ffb2f7f549364
robert-vo <robertvo79@gmail.com>
2016-08-11 03:09:00 -0500

    Added constants and opted to use final variables for the case conditions

--- src/main/java/com/scraper/main/util/StringSQLQueryUtility.java
+++ src/main/java/com/scraper/main/util/StringSQLQueryUtility.java
@@ -20,13 +20,23 @@ public class StringSQLQueryUtility {
     final static String BUILDING_COLUMN             = " building_abbreviation ";
     final static String CREDIT_HOURS_COLUMN         = " credit_hours ";
     final static String CORE_COLUMN                 = " core ";
-    final static String ONLINE                      = "'ONLINE'";
-    final static String HYBRID                      = "'HYBRID'";
-    final static String FACE_TO_FACE                = "'Face To Face'";
-    final static String OPEN                        = "'Open'";
-    final static String CLOSED                      = "'Closed'";
-    final static String REGULAR_ACADEMIC_SESSION    = "'Regular Academic Session'";
-    final static String MINI_SESSION                = "'MIN'";
+    final static String ONLINE                      = "ONLINE";
+    final static String HYBRID                      = "HYBRID";
+    final static String FACE_TO_FACE_PARAM          = "FACETOFACE";
+    final static String FACE_TO_FACE_VALUE          = "Face To Face";
+    final static String STATUS                      = "STATUS";
+    final static String SESSION                     = "SESSION";
+    final static String OPEN                        = "Open";
+    final static String CLOSED                      = "Closed";
+    final static String DEPARTMENT                  = "DEPARTMENT";
+    final static String DEPARTMENT_CRN              = "DEPARTMENT_CRN";
+    final static String LOCATION                    = "LOCATION";
+    final static String COMPONENT                   = "COMPONENT";
+    final static String BUILDING                    = "BUILDING";
+    final static String CREDIT_HOURS                = "CREDIT_HOURS";
+    final static String CORE                        = "CORE";
+    final static String REGULAR_ACADEMIC_SESSION    = "Regular Academic Session";
+    final static String MINI_SESSION                = "MIN";
     final static String SQL_QUERY_FOR_ALL_TERMS     = "SELECT * FROM class, building, department, terms, class_information " +
             "WHERE class.TERM_ID = ? AND " +
             "building.building_abbreviation = class.building_abbv AND " +
@@ -34,7 +44,7 @@ public class StringSQLQueryUtility {
             "terms.term_id = class.term_id AND " +
             "class.department_crn = class_information.department_crn";
 
-    public static String buildSqlQuery(Map<String, String> params) {
+    public static String buildSqlQuery(Map<String, String> params) throws Exception {
 
         StringBuilder sqlQuery = new StringBuilder(SQL_QUERY_FOR_ALL_TERMS);
 
@@ -43,7 +53,7 @@ public class StringSQLQueryUtility {
             String paramValue = params.get(s);
 
             switch (s.toUpperCase()) {
-                case "ONLINE":
+                case ONLINE:
                     if (paramValue.matches(TRUE_VALUES)) {
                         sqlQuery.append(createStringFromColumnConditionValue(FORMAT_COLUMN, EQUALS, ONLINE));
                     }
@@ -51,123 +61,52 @@ public class StringSQLQueryUtility {
                         sqlQuery.append(createStringFromColumnConditionValue(FORMAT_COLUMN, NOT_EQUALS, ONLINE));
                     }
                     break;
-                case "HYBRID":
+                case HYBRID:
                     if (paramValue.matches(TRUE_VALUES)) {
-                        sqlQuery.append(AND)
-                                .append(FORMAT_COLUMN)
-                                .append(EQUALS)
-                                .append(HYBRID);
+                        sqlQuery.append(createStringFromColumnConditionValue(FORMAT_COLUMN, EQUALS, HYBRID));
                     }
                     else if (paramValue.matches(FALSE_VALUES)) {
-                        sqlQuery.append(AND)
-                                .append(FORMAT_COLUMN)
-                                .append(NOT_EQUALS)
-                                .append(HYBRID);
+                        sqlQuery.append(createStringFromColumnConditionValue(FORMAT_COLUMN, NOT_EQUALS, HYBRID));
                     }
                     break;
-                case "FACETOFACE":
+                case FACE_TO_FACE_PARAM:
                     if (paramValue.matches(TRUE_VALUES)) {
-                        sqlQuery.append(AND)
-                                .append(FORMAT_COLUMN)
-                                .append(EQUALS)
-                                .append(FACE_TO_FACE);
+                        sqlQuery.append(createStringFromColumnConditionValue(FORMAT_COLUMN, EQUALS, FACE_TO_FACE_VALUE));
                     }
                     else if (paramValue.matches(FALSE_VALUES)) {
-                        sqlQuery.append(AND)
-                                .append(FORMAT_COLUMN)
-                                .append(NOT_EQUALS)
-                                .append(FACE_TO_FACE);
+                        sqlQuery.append(createStringFromColumnConditionValue(FORMAT_COLUMN, NOT_EQUALS, FACE_TO_FACE_VALUE));
                     }
                     break;
-                case "STATUS":
+                case STATUS:
                     if (paramValue.matches(TRUE_VALUES + "|open")) {
-                        sqlQuery.append(AND)
-                                .append(STATUS_COLUMN)
-                                .append(EQUALS)
-                                .append(OPEN);
+                        sqlQuery.append(createStringFromColumnConditionValue(STATUS_COLUMN, EQUALS, OPEN));
                     }
                     else if (paramValue.matches(FALSE_VALUES + "|closed")) {
-                        sqlQuery.append(AND)
-                                .append(STATUS_COLUMN)
-                                .append(EQUALS)
-                                .append(CLOSED);
+                        sqlQuery.append(createStringFromColumnConditionValue(STATUS_COLUMN, EQUALS, CLOSED));
                     }
                     break;
-                case "SESSION":
-                    switch (paramValue) {
-                        case "1":
-                            sqlQuery.append(AND)
-                                    .append(SESSION_COLUMN)
-                                    .append(EQUALS)
-                                    .append(REGULAR_ACADEMIC_SESSION);
-                            break;
-                        case "MIN":
-                            sqlQuery.append(AND)
-                                    .append(SESSION_COLUMN)
-                                    .append(EQUALS)
-                                    .append(MINI_SESSION);
-                            break;
-                        case "2":
-                        case "3":
-                        case "4":
-                        case "5":
-                        case "6":
-                            sqlQuery.append(AND)
-                                    .append(SESSION_COLUMN)
-                                    .append(EQUALS)
-                                    .append(paramValue);
-                            break;
-                    }
+                case SESSION:
+                    sqlQuery.append(getStringForSessionQuery(SESSION_COLUMN, EQUALS, paramValue));
                     break;
-                case "DEPARTMENT":
-                    sqlQuery.append(AND)
-                            .append(DEPARTMENT_COLUMN)
-                            .append(EQUALS)
-                            .append("'")
-                            .append(paramValue)
-                            .append("'");
+                case DEPARTMENT:
+                    sqlQuery.append(createStringFromColumnConditionValue(DEPARTMENT_COLUMN, EQUALS, paramValue));
                     break;
-                case "DEPARTMENT_CRN":
-                    sqlQuery.append(AND)
-                            .append(DEPARTMENT_CRN_COLUMN)
-                            .append(EQUALS)
-                            .append("'")
-                            .append(paramValue)
-                            .append("'");
+                case DEPARTMENT_CRN:
+                    sqlQuery.append(createStringFromColumnConditionValue(DEPARTMENT_CRN_COLUMN, EQUALS, paramValue));
                     break;
-                case "LOCATION":
-                    sqlQuery.append(AND)
-                            .append(LOCATION_COLUMN)
-                            .append(EQUALS)
-                            .append("'")
-                            .append(paramValue)
-                            .append("'");
+                case LOCATION:
+                    sqlQuery.append(createStringFromColumnConditionValue(LOCATION_COLUMN, EQUALS, paramValue));
                     break;
-                case "COMPONENT":
-                    sqlQuery.append(AND)
-                            .append(COMPONENT_COLUMN)
-                            .append(EQUALS)
-                            .append("'")
-                            .append(paramValue)
-                            .append("'");
+                case COMPONENT:
+                    sqlQuery.append(createStringFromColumnConditionValue(COMPONENT_COLUMN, EQUALS, paramValue));
                     break;
-                case "BUILDING":
-                    sqlQuery.append(AND)
-                            .append(BUILDING_COLUMN)
-                            .append(EQUALS)
-                            .append("'")
-                            .append(paramValue)
-                            .append("'");
+                case BUILDING:
+                    sqlQuery.append(createStringFromColumnConditionValue(BUILDING_COLUMN, EQUALS, paramValue));
                     break;
-                case "CREDIT_HOURS":
-                    sqlQuery.append(AND)
-                            .append(CREDIT_HOURS_COLUMN)
-                            .append(EQUALS)
-                            .append("'")
-                            .append(paramValue)
-                            .append("'");
+                case CREDIT_HOURS:
+                    sqlQuery.append(createStringFromColumnConditionValue(CREDIT_HOURS_COLUMN, EQUALS, paramValue));
                     break;
-                case "CORE":
+                case CORE:
                     sqlQuery.append(AND)
                             .append(" (core = ")
                             .append(paramValue)
@@ -185,6 +124,22 @@ public class StringSQLQueryUtility {
     }
 
     private static String createStringFromColumnConditionValue(String column, String condition, String paramValue) {
-        return AND + column + condition + paramValue;
+        return AND + column + condition + "'" + paramValue + "'";
+    }
+
+    private static String getStringForSessionQuery(String column, String condition, String paramValue) throws Exception {
+        switch (paramValue) {
+            case "1":
+                return createStringFromColumnConditionValue(SESSION_COLUMN, EQUALS, REGULAR_ACADEMIC_SESSION);
+            case "MIN":
+                return createStringFromColumnConditionValue(SESSION_COLUMN, EQUALS, MINI_SESSION);
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+                return createStringFromColumnConditionValue(SESSION_COLUMN, EQUALS, paramValue);
+        }
+        throw new Exception("Invalid key for session query.");
     }
 }
