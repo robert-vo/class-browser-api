6e1a5d529fab07e48d60ac2afaa880660eb454d3
robert-vo <robertvo79@gmail.com>
2016-08-13 23:35:00 -0500

    Initial Commit - New Exception to handle invalid parameters

---
+++ src/main/java/com/scraper/main/InvalidArgumentException.java
@@ -0,0 +1,13 @@
+package com.scraper.main;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid Argument")
+public class InvalidArgumentException extends Exception {
+
+    public InvalidArgumentException(String parameter){
+        super("InvalidArgumentException with parameter of: " + parameter);
+    }
+
+}
