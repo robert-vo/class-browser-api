f7c2e51f18fbff597b82f26f96f90af7a8f1793e
robert-vo <robertvo79@gmail.com>
2016-08-16 10:56:25 -0500

    Removed fields and constructor. Refactored catching of Exceptions. Refactored getAllClassesFromTerm to use DAO. Added attemptDatabaseOperation to reduce duplication of code. Added generateErrorMessage to reduce duplication of code.

--- src/main/java/com/scraper/main/ClassScraperAPIController.java
+++ src/main/java/com/scraper/main/ClassScraperAPIController.java
@@ -1,61 +1,33 @@
 package com.scraper.main;
 
-import com.scraper.main.util.StringSQLQueryUtility;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.*;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-
-import static com.scraper.main.ClassInformation.getClassEntryFromResultSet;
 
 @RestController
 @RequestMapping("/api")
 public class ClassScraperAPIController {
 
-    static Properties properties = new Properties();
-    static String jdbcDriver;
-    static String databaseURL;
-    static String databaseTable;
-    static String userName;
-    static String passWord;
-
-    ClassScraperAPIController() throws IOException {
-        setDatabaseConfigurations();
-    }
-
-    private void setDatabaseConfigurations() throws IOException {
-        InputStream input = new FileInputStream("config/config.properties");
-        properties.load(input);
-        jdbcDriver      = properties.getProperty("jdbcDriver");
-        databaseTable   = properties.getProperty("databaseTable");
-        databaseURL     = properties.getProperty("databaseURL") +  "/" + databaseTable;
-        userName        = properties.getProperty("userName");
-        passWord        = properties.getProperty("passWord");
-    }
-
     @RequestMapping(value = "/core={core}", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity getAllCoreCourses(@PathVariable(value = "core") String core) throws SQLException, IOException {
+    public ResponseEntity getAllCoreCourses(@PathVariable(value = "core") String core) throws Exception {
         try {
             if(ClassInformation.isNotValidCore(core)) {
                 throw new InvalidArgumentException("Core");
             }
         }
         catch (NumberFormatException | InvalidArgumentException e) {
-            ErrorMessage errorMessage = new ErrorMessage("Core");
-            return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+            return generateErrorMessage("Core");
         }
 
         CoreClassInformationDAO coreClassInformationDAO = new CoreClassInformationDAO();
-        return new ResponseEntity<>(coreClassInformationDAO.getFromDatabaseAndResponseInfo(core), HttpStatus.OK);
+        Map<String, String> params = new HashMap<>();
+        params.put("Core", core);
+
+        return attemptDatabaseOperation(coreClassInformationDAO, params);
     }
 
     @RequestMapping(value = "/term={term}", method = RequestMethod.GET)
@@ -63,47 +35,54 @@ public class ClassScraperAPIController {
     public ResponseEntity getAllClassesFromTerm(@PathVariable(value = "term") String term,
                                                 @RequestParam Map<String, String> params) throws Exception {
 
-        List<ClassInformation> allClassInformation = new LinkedList<>();
-        final String SQL_QUERY_ALL_CLASSES = StringSQLQueryUtility.buildSqlQuery(params);
-
-        if(ClassInformation.isNotValidTerm(term)) {
-            try {
+        try {
+            if (term.isEmpty()) {
+                throw new Exception("Term is required.");
+            }
+            else if (ClassInformation.isNotValidTerm(term)) {
                 throw new InvalidArgumentException("Term");
             }
-            catch (InvalidArgumentException e) {
-                ErrorMessage errorMessage = new ErrorMessage("Term");
-                return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
-            }
         }
-        handleJavaLangClassDriver();
-
-        ResponseInformation responseInformation = null;
-        try (Connection conn = DriverManager.getConnection(databaseURL, userName, passWord)) {
-            PreparedStatement preparedStatement = conn.prepareStatement(SQL_QUERY_ALL_CLASSES);
-            preparedStatement.setString(1, term);
-            System.out.println("Running query - " + preparedStatement.toString());
-            ResultSet resultSet = preparedStatement.executeQuery();
-            while (resultSet.next()) {
-                ClassInformation c = getClassEntryFromResultSet(resultSet);
-                allClassInformation.add(c);
-            }
-
-            resultSet.last();
-            params.put("term", term);
-            responseInformation = new ResponseInformation(resultSet.getRow(), params, allClassInformation);
-        } catch (SQLException e) {
-            e.printStackTrace();
+        catch (Exception e) {
+            return generateErrorMessage(e);
         }
 
-        return new ResponseEntity<>(new ClassResponseInformation(responseInformation, allClassInformation), HttpStatus.OK);
+        ClassInformationDAO classInformationDAO = new ClassInformationDAO();
+        params.put("Term", term);
+
+        return attemptDatabaseOperation(classInformationDAO, params);
     }
 
-    private void handleJavaLangClassDriver() {
+    private ResponseEntity attemptDatabaseOperation(Object DAO, Map params) {
         try {
-            java.lang.Class.forName(jdbcDriver);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            if (DAO instanceof ClassInformationDAO) {
+                final ClassInformationDAO classInformationDAO = (ClassInformationDAO) DAO;
+                return new ResponseEntity<>(classInformationDAO.getFromDatabaseAndResponseInfo(params), HttpStatus.OK);
+            }
+            else if (DAO instanceof CoreClassInformationDAO) {
+                final CoreClassInformationDAO coreClassInformationDAO = (CoreClassInformationDAO) DAO;
+                return new ResponseEntity<>(coreClassInformationDAO.getFromDatabaseAndResponseInfo(params), HttpStatus.OK);
+            }
+            else {
+                throw new Exception("Invalid Operation on DAO " + DAO.getClass());
+            }
+        }
+        catch (Exception e) {
+            return generateErrorMessage(e);
         }
     }
 
+    private ResponseEntity generateErrorMessage(Object param) {
+        ErrorMessage errorMessage;
+        if(param instanceof Exception) {
+            errorMessage = new ErrorMessage((Exception) param);
+        }
+        else if (param instanceof String) {
+            errorMessage = new ErrorMessage((String) param);
+        }
+        else {
+            errorMessage = new ErrorMessage(new Exception("Invalid Object"));
+        }
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
 }
