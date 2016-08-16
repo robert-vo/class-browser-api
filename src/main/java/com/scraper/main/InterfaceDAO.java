40d9fc68768007f48ca986188dfcf4ead778ae79
robert-vo <robertvo79@gmail.com>
2016-08-16 10:02:34 -0500

    Added method to interface

--- src/main/java/com/scraper/main/InterfaceDAO.java
+++ src/main/java/com/scraper/main/InterfaceDAO.java
@@ -3,11 +3,17 @@ package com.scraper.main;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map;
 
 public interface InterfaceDAO<T> {
     void setDatabaseInformation() throws IOException;
-    T retrieveFromResultSet(ResultSet rs) throws SQLException;
-    T processStringQuery(String sqlQuery, String param) throws SQLException;
+
     void handleJavaLangClassDriver();
+
+    T retrieveFromResultSet(ResultSet rs) throws SQLException;
+
+    T processStringQuery(String sqlQuery, String param) throws SQLException;
+
+    T getFromDatabaseAndResponseInfo(Map allParams) throws SQLException;
 }
 
