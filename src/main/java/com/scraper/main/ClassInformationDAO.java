2bc774e5f65d61b26a564866ca0240b5daf9e147
robert-vo <robertvo79@gmail.com>
2016-08-16 10:54:37 -0500

    Implemented the methods in the interface and added a method to select all classes

--- src/main/java/com/scraper/main/ClassInformationDAO.java
+++ src/main/java/com/scraper/main/ClassInformationDAO.java
@@ -1,28 +1,83 @@
 package com.scraper.main;
 
+import com.scraper.main.util.StringSQLQueryUtility;
+
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 public class ClassInformationDAO implements InterfaceDAO {
 
+    static Properties properties = new Properties();
+    static String jdbcDriver;
+    static String databaseURL;
+    static String databaseTable;
+    static String userName;
+    static String passWord;
+
+    ClassInformationDAO() throws IOException {
+        setDatabaseInformation();
+        handleJavaLangClassDriver();
+    }
+
     @Override
     public void setDatabaseInformation() throws IOException {
-
-    }
-
-    @Override
-    public Object retrieveFromResultSet(ResultSet rs) throws SQLException {
-        return null;
-    }
-
-    @Override
-    public Object processStringQuery(String sqlQuery, String param) throws SQLException {
-        return null;
+        InputStream input = new FileInputStream("config/config.properties");
+        properties.load(input);
+        jdbcDriver      = properties.getProperty("jdbcDriver");
+        databaseTable   = properties.getProperty("databaseTable");
+        databaseURL     = properties.getProperty("databaseURL") +  "/" + databaseTable;
+        userName        = properties.getProperty("userName");
+        passWord        = properties.getProperty("passWord");
     }
 
     @Override
     public void handleJavaLangClassDriver() {
+        try {
+            java.lang.Class.forName(jdbcDriver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    public List<ClassInformation> retrieveFromResultSet(ResultSet rs) throws SQLException {
+        List<ClassInformation> allClassInformation = new LinkedList<>();
+        while(rs.next()) {
+            ClassInformation c = ClassInformation.getClassEntryFromResultSet(rs);
+            allClassInformation.add(c);
+        }
+        return allClassInformation;
+    }
+
+    @Override
+    public List<ClassInformation> processStringQuery(String sqlQuery, String param) throws SQLException {
+        try(Connection conn = DriverManager.getConnection(databaseURL, userName, passWord)) {
+            PreparedStatement preparedStatement = conn.prepareStatement(sqlQuery);
+            preparedStatement.setString(1, param);
+            System.out.println(preparedStatement);
+            return retrieveFromResultSet(preparedStatement.executeQuery());
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public ResponseInformation<List<ClassInformation>> getFromDatabaseAndResponseInfo(Map allParams) throws Exception {
+        List<ClassInformation> allClasses = selectAllClasses(allParams);
+        int numberOfRows = allClasses.size();
+        return new ResponseInformation<>(numberOfRows, allParams, allClasses);
+    }
+
+    public List<ClassInformation> selectAllClasses(Map<String, String> allParams) throws Exception {
+        final String SQL_QUERY_ALL_CLASSES = StringSQLQueryUtility.buildSqlQuery(allParams);
+        return processStringQuery(SQL_QUERY_ALL_CLASSES, allParams.get("Term"));
     }
 }
