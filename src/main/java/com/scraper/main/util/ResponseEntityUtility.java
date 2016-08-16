a20b4104bae7d5198e81a407922cbce3c3594ed7
robert-vo <robertvo79@gmail.com>
2016-08-16 11:02:09 -0500

    Method ResponseEntity logic from the APIController to this class, ResponseEntityUtility.

---
+++ src/main/java/com/scraper/main/util/ResponseEntityUtility.java
@@ -0,0 +1,44 @@
+package com.scraper.main.util;
+
+import com.scraper.main.ClassInformationDAO;
+import com.scraper.main.CoreClassInformationDAO;
+import com.scraper.main.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public class ResponseEntityUtility {
+    public static ResponseEntity attemptDatabaseOperation(Object DAO, Map params) {
+        try {
+            if (DAO instanceof ClassInformationDAO) {
+                final ClassInformationDAO classInformationDAO = (ClassInformationDAO) DAO;
+                return new ResponseEntity<>(classInformationDAO.getFromDatabaseAndResponseInfo(params), HttpStatus.OK);
+            }
+            else if (DAO instanceof CoreClassInformationDAO) {
+                final CoreClassInformationDAO coreClassInformationDAO = (CoreClassInformationDAO) DAO;
+                return new ResponseEntity<>(coreClassInformationDAO.getFromDatabaseAndResponseInfo(params), HttpStatus.OK);
+            }
+            else {
+                throw new Exception("Invalid Operation on DAO " + DAO.getClass());
+            }
+        }
+        catch (Exception e) {
+            return generateErrorMessageResponseEntity(e);
+        }
+    }
+
+    public static ResponseEntity generateErrorMessageResponseEntity(Object param) {
+        ErrorMessage errorMessage;
+        if(param instanceof Exception) {
+            errorMessage = new ErrorMessage((Exception) param);
+        }
+        else if (param instanceof String) {
+            errorMessage = new ErrorMessage((String) param);
+        }
+        else {
+            errorMessage = new ErrorMessage(new Exception("Invalid Object"));
+        }
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+}
