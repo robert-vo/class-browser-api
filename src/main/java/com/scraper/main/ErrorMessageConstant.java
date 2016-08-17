f86cba8868ae455cb149ca18f3bd061b67bbd29c
robert-vo <robertvo79@gmail.com>
2016-08-17 10:43:24 -0500

    Removed comments

--- src/main/java/com/scraper/main/ErrorMessageConstant.java
+++ src/main/java/com/scraper/main/ErrorMessageConstant.java
@@ -1,8 +1,5 @@
 package com.scraper.main;
 
-/**
- * Created by Robert on 8/14/16.
- */
 public enum ErrorMessageConstant {
     CORE ("Core must be a numeric value between 1 and 10, inclusive."),
     TERM ("Terms must be an integer starting from 1970 and incrementing by 10.");
