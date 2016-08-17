6d96fea66f87ebba71373ccfda54ae0f4d29d836
robert-vo <robertvo79@gmail.com>
2016-08-17 10:42:48 -0500

    Added two fields - statusCode and message - to the POJO

--- src/main/java/com/scraper/main/ResponseInformation.java
+++ src/main/java/com/scraper/main/ResponseInformation.java
@@ -3,14 +3,34 @@ package com.scraper.main;
 import java.util.Map;
 
 public class ResponseInformation<T> {
+    private int statusCode;
+    private String message;
     private int numberOfRows;
     private Map<String, String> parameters;
     private T result;
 
-    public ResponseInformation(int numberOfRows, Map<String, String> parameters, T result) {
+    public ResponseInformation(int statusCode, String message, int numberOfRows, Map<String, String> parameters, T result) {
+        this.statusCode = statusCode;
+        this.message = message;
         this.numberOfRows = numberOfRows;
         this.parameters = parameters;
         this.result = result;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 
     public int getNumberOfRows() {
