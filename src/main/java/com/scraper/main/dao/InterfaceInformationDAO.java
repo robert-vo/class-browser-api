c5f9bae88809c4dfb496d557793fb2c0627a3305
robert-vo <robertvo79@gmail.com>
2016-08-18 23:59:29 -0500

    Renamed file and removed two methods which are now handled by the abstract class

--- src/main/java/com/scraper/main/dao/InterfaceInformationDAO.java
+++ src/main/java/com/scraper/main/dao/InterfaceInformationDAO.java
@@ -1,15 +1,10 @@
 package com.scraper.main.dao;
 
-import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
 
-public interface InterfaceDAO<T> {
-    void setDatabaseInformation() throws IOException;
-
-    void handleJavaLangClassDriver();
-
+public interface InterfaceInformationDAO<T> {
     T retrieveFromResultSet(ResultSet rs) throws SQLException;
 
     T processStringQuery(String sqlQuery, String param) throws SQLException;
