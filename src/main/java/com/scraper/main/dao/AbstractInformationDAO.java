6a4fa08a04ce787c1f4ccc5bb91f62a94d5a4fa0
robert-vo <robertvo79@gmail.com>
2016-08-18 23:58:13 -0500

    Initial commit with abstract class that handles logic that was previously duplicated

---
+++ src/main/java/com/scraper/main/dao/AbstractInformationDAO.java
@@ -0,0 +1,38 @@
+package com.scraper.main.dao;
+
+import java.util.Optional;
+
+public abstract class AbstractInformationDAO {
+    String jdbcDriver;
+    String databaseURL;
+    String databaseTable;
+    String userName;
+    String passWord;
+
+    AbstractInformationDAO() {
+        setDatabaseInformation();
+        handleJavaLangClassDriver();
+    }
+
+    public void setDatabaseInformation() {
+        Optional<String> jdbcDriverProperty = Optional.ofNullable(System.getProperty("jdbcDriver"));
+        Optional<String> databaseTableProperty = Optional.ofNullable(System.getProperty("databaseTable"));
+        Optional<String> databaseURLProperty = Optional.ofNullable(System.getProperty("databaseURL"));
+        Optional<String> userNameProperty = Optional.ofNullable(System.getProperty("databaseUserName"));
+        Optional<String> passWordProperty = Optional.ofNullable(System.getProperty("databasePassWord"));
+
+        jdbcDriver      = jdbcDriverProperty.isPresent() ? System.getProperty("jdbcDriver") : "com.mysql.jdbc.Driver";
+        databaseTable   = databaseTableProperty.isPresent() ? System.getProperty("databaseTable") : "class";
+        databaseURL     = databaseURLProperty.isPresent() ? System.getProperty("databaseURL") + "/" + databaseTable : "jdbc:mysql://localhost" + "/" + databaseTable;
+        userName        = userNameProperty.isPresent() ? System.getProperty("databaseUserName") : "root";
+        passWord        = passWordProperty.isPresent() ? System.getProperty("databasePassWord") : "password";
+    }
+
+    public void handleJavaLangClassDriver() {
+        try {
+            java.lang.Class.forName(jdbcDriver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
