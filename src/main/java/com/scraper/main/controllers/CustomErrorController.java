152b3b8f40aaf0eab1fb05549512fe53f3a0989e
robert-vo <robertvo79@gmail.com>
2016-08-18 22:31:28 -0500

    New Controller to handle invalid pages

---
+++ src/main/java/com/scraper/main/controllers/CustomErrorController.java
@@ -0,0 +1,23 @@
+package com.scraper.main.controllers;
+
+import com.scraper.main.pojo.ErrorMessage;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomErrorController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    ErrorMessage error() {
+        return new ErrorMessage("Error");
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+
+}
