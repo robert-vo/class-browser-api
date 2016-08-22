244a2e5da6b758782a67b1b40aac113ce3bb9ae5
robert-vo <robertvo79@gmail.com>
2016-08-22 16:29:11 -0500

    Initial commit - new DAOImpl.

---
+++ src/main/java/com/classbrowser/main/dao/ClassInformationDaoImpl.java
@@ -0,0 +1,54 @@
+package com.classbrowser.main.dao;
+
+import com.classbrowser.main.commons.util.StringSQLQueryUtility;
+import com.classbrowser.main.pojo.ClassInformation;
+import com.classbrowser.main.pojo.ResponseInformation;
+import org.apache.log4j.Logger;
+
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class ClassInformationDaoImpl extends AbstractInformationDAO implements ClassInformationDAO {
+
+    private static Logger log = Logger.getLogger(ClassInformationDaoImpl.class);
+
+    @Override
+    public List<ClassInformation> retrieveFromResultSet(ResultSet rs) throws SQLException {
+        List<ClassInformation> allClassInformation = new LinkedList<>();
+        while(rs.next()) {
+            ClassInformation c = ClassInformation.getCoreClassFromResultSet(rs);
+            allClassInformation.add(c);
+        }
+        return allClassInformation;
+    }
+
+    @Override
+    public List<ClassInformation> processStringQuery(String sqlQuery, String param) throws SQLException {
+        try(Connection conn = DriverManager.getConnection(databaseURL, userName, passWord)) {
+            PreparedStatement preparedStatement = conn.prepareStatement(sqlQuery);
+            log.info("Executing SQL Query: " + preparedStatement.toString());
+            return retrieveFromResultSet(preparedStatement.executeQuery());
+        }
+        catch (Exception e) {
+            log.error("Error processing SQL Query");
+            log.error(e);
+        }
+        return null;
+    }
+
+    @Override
+    public ResponseInformation<List<ClassInformation>> getFromDatabaseAndResponseInfo(Map allParams) throws Exception {
+        List<ClassInformation> allClasses = selectAllClassInformation(allParams);
+        int numberOfRows = allClasses.size();
+        log.info("Retrieved " + numberOfRows + ".");
+        return new ResponseInformation<>(numberOfRows, allParams, allClasses);
+    }
+
+    @Override
+    public List<ClassInformation> selectAllClassInformation(Map allParams) throws Exception {
+        final String SQL_QUERY_ALL_CLASSES = StringSQLQueryUtility.buildSqlQueryForInformation(allParams);
+        return processStringQuery(SQL_QUERY_ALL_CLASSES, "");
+    }
+}
