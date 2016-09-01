7a6576294cdbbb37e2fb9bd86cf6307d142f9ad0
robert-vo <robertvo79@gmail.com>
2016-08-31 22:26:47 -0500

    Added documentation.

--- src/main/java/com/classbrowser/main/dao/InterfaceInformationDAO.java
+++ src/main/java/com/classbrowser/main/dao/InterfaceInformationDAO.java
@@ -5,8 +5,20 @@ import java.sql.SQLException;
 
 /**
  * Data Access Object interface with the expectation that all concrete POJO DAOs will implement these methods.
+ *
+ * @author Robert Vo
  */
 public interface InterfaceInformationDAO<T> {
+    /**
+     * Constructs a prepared statement from the sql query, sets a parameter in the prepared statement
+     * using parameter, param, executes the sql query and returns the result.
+     * @param sqlQuery
+     * @param param
+     * @return
+     * @throws SQLException
+     */
+    T processStringQuery(String sqlQuery, String... param) throws SQLException;
+
     /**
      * Iterates through the result of the SQL query and stores each row into an object T.
      *
@@ -16,13 +28,4 @@ public interface InterfaceInformationDAO<T> {
      */
     T retrieveFromResultSet(ResultSet rs) throws SQLException;
 
-    /**
-     * Constructs a prepared statement from the sql query, sets a parameter in the prepared statement
-     * using {@param} param, executes the sql query and returns the result.
-     * @param sqlQuery
-     * @param param
-     * @return
-     * @throws SQLException
-     */
-    T processStringQuery(String sqlQuery, String... param) throws SQLException;
 }
