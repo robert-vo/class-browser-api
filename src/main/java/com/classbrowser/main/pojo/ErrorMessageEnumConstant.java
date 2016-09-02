6efa3c7d797393cbef57ccac1941c0ad2a0c5b8d
robert-vo <robertvo79@gmail.com>
2016-09-02 02:20:06 -0500

    Edited error message.

--- src/main/java/com/classbrowser/main/pojo/ErrorMessageEnumConstant.java
+++ src/main/java/com/classbrowser/main/pojo/ErrorMessageEnumConstant.java
@@ -9,7 +9,7 @@ public enum ErrorMessageEnumConstant {
     CORE ("Core must be a numeric value between 1 and 10, inclusive."),
     TERM ("Terms must be an integer starting from 1970 and incrementing by 10."),
     CREDIT_HOUR ("Credit Hours must be an integer from 1 to 9"),
-    ERROR ("There has seem to be an error with the URL requested. Please consult check out the API documentation at <https://github.com/robert-vo/class-browser-api>.");
+    ERROR ("There has seem to be an error with the URL requested. Please consult the API documentation at <https://github.com/robert-vo/class-browser-api>.");
 
     private final String message;
 
