220e5cc3afed4060dc427b87a897453f219d0963
robert-vo <robertvo79@gmail.com>
2016-09-02 00:55:14 -0500

    Updated documentation and changed return type from T to List<T> and changed variable name from sqlQuery to baseSQLQuery.

--- src/main/java/com/classbrowser/main/dao/InterfaceInformationDAO.java
+++ src/main/java/com/classbrowser/main/dao/InterfaceInformationDAO.java
@@ -2,6 +2,7 @@ package com.classbrowser.main.dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Data Access Object interface with the expectation that all concrete POJO DAOs will implement these methods.
@@ -12,20 +13,22 @@ public interface InterfaceInformationDAO<T> {
     /**
      * Constructs a prepared statement from the sql query, sets a parameter in the prepared statement
      * using parameter, param, executes the sql query and returns the result.
-     * @param sqlQuery
-     * @param param
-     * @return
-     * @throws SQLException
+     * @param baseSQLQuery The base SQL query to be executed.
+     * @param param Additional parameters to be applied to the base SQL query.
+     * @return A List of object T where each entry in the list represents a row in the ResultSet.
+     * @throws SQLException When a problem occurs with setting up the database or performing the SQL query.
      */
-    T processStringQuery(String sqlQuery, String... param) throws SQLException;
+    List<T> processStringQuery(String baseSQLQuery, String... param) throws SQLException;
 
     /**
      * Iterates through the result of the SQL query and stores each row into an object T.
      *
+     * This serves as a helper method for processStringQuery.
+     *
      * @param rs - The result of the sql query.
-     * @return A list of object T where each entry in the list represents a row in the ResultSet.
-     * @throws SQLException
+     * @return A List of object T where each entry in the list represents a row in the ResultSet.
+     * @throws SQLException When a problem occurs with setting up the database or performing the SQL query.
      */
-    T retrieveFromResultSet(ResultSet rs) throws SQLException;
+    List<T> retrieveFromResultSet(ResultSet rs) throws SQLException;
 
 }
