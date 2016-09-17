7906d37339a354d4ea01ac9a12794a04483b12f2
robert-vo <robertvo79@gmail.com>
2016-09-16 22:26:13 -0500

    Added an additional localhost domain for CrossOrigin.

--- src/main/java/com/classbrowser/main/controllers/ClassBrowserAPIController.java
+++ src/main/java/com/classbrowser/main/controllers/ClassBrowserAPIController.java
@@ -24,7 +24,7 @@ import static com.classbrowser.main.commons.util.ResponseEntityUtility.generateE
  * @author Robert Vo
  */
 //TODO: Update second origins domain to hosted website server.
-@CrossOrigin(origins = {"http://localhost:63343", "classbrowseruh.us-west-2.elasticbeanstalk.com/api"})
+@CrossOrigin(origins = {"http://localhost:63342", "http://localhost:63343", "classbrowseruh.us-west-2.elasticbeanstalk.com/api"})
 @RestController
 @RequestMapping("/api")
 public class ClassBrowserAPIController {
